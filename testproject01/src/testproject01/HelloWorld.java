c454c59fbb0085328801574f476df1bce55c8b72
itou-yuria <m.imi@outlook.jp>
2019-10-25 17:44:07 +0900

    2019/10/25

---
+++ testproject01/src/testproject01/HelloWorld.java
@@ -0,0 +1,12 @@
+package testproject01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i =10;
+		do{
+			System.out.println(i);
+			i-=3;
+		}while(i>=1);
+	}}
